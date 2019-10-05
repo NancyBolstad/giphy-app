bb9782239227363c613e3bd354138c7a9e02a832
NancyBolstad <litchacha@gmail.com>
2019-10-05 20:38:52 +0200

    Removed button

--- src/components/ColorButton/story.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import ColorButton from '.';
-
-storiesOf('Component/ColorButton', module)
-  .add('red', () => <ColorButton color="red" />)
-  .add('blue', () => <ColorButton color="blue" />);
