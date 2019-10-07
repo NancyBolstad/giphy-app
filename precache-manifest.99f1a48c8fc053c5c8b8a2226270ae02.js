5b55f0a88d23b4d2e32e715061eb21903d38f4a4
NancyBolstad <litchacha@gmail.com>
2019-10-07 12:55:59 +0200

    Updates

---
+++ precache-manifest.99f1a48c8fc053c5c8b8a2226270ae02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7199f3c8777afa53014fdc040ec433",
+    "url": "/giphy-app/index.html"
+  },
+  {
+    "revision": "4d864fce0f537d87c327",
+    "url": "/giphy-app/static/css/main.11b583f1.chunk.css"
+  },
+  {
+    "revision": "57ec454c3565d7afce70",
+    "url": "/giphy-app/static/js/2.930345ae.chunk.js"
+  },
+  {
+    "revision": "4d864fce0f537d87c327",
+    "url": "/giphy-app/static/js/main.d7d23264.chunk.js"
+  },
+  {
+    "revision": "aa92f624d71e057f76b3",
+    "url": "/giphy-app/static/js/runtime-main.48a8cca3.js"
+  }
+]);
